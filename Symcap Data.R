19ad5b3d508b4798d21f1312da8365123605feda
Teegan Innis <sportsguy1033@Teegan-Inniss-MacBook-Pro.local>
2016-07-22 13:40:13 -1000

    qPCR update

--- Symcap Data.R
+++ Symcap Data.R
@@ -6,7 +6,6 @@ library(reshape2)
 #import collection data
 Coral_Data <- read.csv("Coral_Collection.csv")
 Coral_Data$Depth..m. <- as.numeric(as.character(Coral_Data$Depth..m.))
-View(Coral_Data)
 
 #analyzing depth distribution
 hist(Coral_Data$Depth..m.)
@@ -18,19 +17,6 @@ subset(Coral_Data, Reef.Area!="Top")
 Slope <- subset(Coral_Data, Reef.Area!="Top")
 hist(Slope$Depth..m.)
 
-#import gps data
-GPSData <- read.csv("GPS_Data.csv", skip = 27, stringsAsFactors = F)
-GPSData$ID <- as.numeric(as.character(GPSData$ID))
-GPSData <- GPSData[GPSData$ID>158,]
-stopat <- which(GPSData$ID=="")[1]  # Find first row where ID is blank
-GPSData <- GPSData[1:stopat-1, ]  # Remove all rows past the first blank row
-View(GPSData)
-
-# Convert time from character object to time object
-GPSData$time <- as.POSIXct(GPSData$time, format="%Y-%m-%dT%H:%M:%SZ", tz="GMT")
-GPSData$hst <- format(GPSData$time, tz="Pacific/Honolulu")
-View(GPSData)
-
 # Importing qPCR data
 source_url("https://raw.githubusercontent.com/jrcunning/steponeR/master/steponeR.R")
 Mcap.plates <- list.files(path = "qPCR_data", pattern = "txt$", full.names = T)
@@ -40,7 +26,6 @@ Mcap <- steponeR(files = Mcap.plates, delim="\t",
                  fluor.norm = list(C=2.26827, D=0), 
                  copy.number=list(C=33, D=3))
 Mcap <- Mcap$result
-View(Mcap)
 
 #Remove positive controls
 Mcap <- Mcap[grep("+", Mcap$Sample.Name, fixed=T, invert = T), ]
@@ -55,7 +40,6 @@ colnames(Mcap)[which(colnames(Mcap)=="Sample.Name")] <- "Colony"
 Mcap$fail <- ifelse(Mcap$C.reps < 2 & Mcap$D.reps < 2, TRUE, FALSE)
 fails <- Mcap[Mcap$fail==TRUE, ]
 Mcap <- Mcap[which(Mcap$fail==FALSE),]
-View(Mcap)
 
 #When C or D detected in only 1 sample, set ratio to +/- Inf
 Mcap$C.D[which(Mcap$C.reps<2)] <--Inf
@@ -63,7 +47,6 @@ Mcap$C.D[which(Mcap$D.reps<2)] <-Inf
 
 #Order by Colony
 Mcap <- Mcap[with(Mcap, order(Colony)), ]
-View(Mcap)
 
 #Proportion C
 Mcap$propC <- Mcap$C.D / (Mcap$C.D+1)
@@ -83,8 +66,12 @@ Symcap<-merge(Coral_Data, Mcap, by="Colony", all=T)
 
 #Chi Squared test for independence
 total=table(Symcap$Color.Morph, Symcap$Dom)
+chisq.test(total)
 total
+Slope <- subset(Symcap, Reef.Area!="Slope")
+Slope$Reef.Area <- droplevels(Slope$Reef.Area)
+total=table(Slope$Reef.Area, Slope$Color.Morph)
 chisq.test(total)
 
 #Mosaic Plot
-mosaicplot(total, ylab = "Color Morph", xlab = "Dominant Symbiont", main = "")
+mosaicplot(total, ylab = "Reef Area", xlab = "Color Morph", main = "")
