c58181bb6d7c2210204fbfa0444e72338fb53996
Teegan Innis <sportsguy1033@Teegan-Inniss-MacBook-Pro.local>
2016-07-29 20:05:23 -1000

    fieldwork data

--- Symcap Data.R
+++ Symcap Data.R
@@ -9,16 +9,6 @@ library(RgoogleMaps)
 Coral_Data <- read.csv("Coral_Collection.csv")
 Coral_Data$Depth..m. <- as.numeric(as.character(Coral_Data$Depth..m.))
 
-#analyzing depth distribution
-hist(Coral_Data$Depth..m.)
-Leeward=subset(Coral_Data, Reef.Area=="Leeward")
-hist(Leeward$Depth..m.)
-Windward=subset(Coral_Data, Reef.Area=="Windward")
-hist(Windward$Depth..m.)
-subset(Coral_Data, Reef.Area!="Top")
-Slope <- subset(Coral_Data, Reef.Area!="Top")
-hist(Slope$Depth..m.)
-
 # Importing qPCR data
 source_url("https://raw.githubusercontent.com/jrcunning/steponeR/master/steponeR.R")
 Mcap.plates <- list.files(path = "qPCR_data", pattern = "txt$", full.names = T)
@@ -67,32 +57,14 @@ Mcap$Dom <- ifelse(Mcap$propC>Mcap$propD, "C", "D")
 Symcap<-merge(Coral_Data, Mcap, by="Colony", all=T)
 
 #Chi Squared test for independence
-total=table(Symcap$Reef.Area, Symcap$Color.Morph)
-chisq.test(total)
-prop.table(total, margin=2)
-Slope <- subset(Symcap, Reef.Area!="Top")
-Slope$Reef.Area <- droplevels(Slope$Reef.Area)
-total=table(Slope$Color.Morph, Slope$Reef.Area)
-chisq.test(total)
-total
-
-Symcap$Slope <- ifelse(Symcap$Reef.Area!="Top", yes = "Slope", no = "Top")
-total=table(Symcap$Color.Morph, Symcap$Slope)
+Symcap$Reef.Area <- ifelse(Symcap$Reef.Area!="Top", yes = "Slope", no = "Top")
+total=table(Symcap$Dom, Symcap$Color.Morph)
 total
 chisq.test(total)
 prop.table(total, margin = 2)
 par(mar=c(3, 4, 2, 6))
 barplot(prop.table(total, margin = 2), col = c("gray20", "gray95"), xlab = "Color Morph", ylab = "Proportion of Dominant Symbiont")
 legend("topright", legend=c("C","D"), fill=c("gray20", "gray95"), inset = c(-.2, 0), xpd = NA)
-
-#Export Image
-pdf(file="Color~Depth.pdf", height = 4, width = 5)
-par(mar=c(0, 4, 0, 4))
-logi.hist.plot(Symcap$Depth..m., Symcap$Color, boxp = FALSE, type = "hist", col="gray", xlabel = "Depth (m)", ylabel = "", ylabel2 = "")
-mtext(side = 4, text = "Frequency", line = 3, cex=1)
-mtext(side = 4, text = "Brown                                Orange", line = 2, cex = 0.75)
-mtext(side = 2, text = "Probability of Brown Color Morph", line = 3, cex = 1)
-dev.off()
 
 #Mosaic Plot
 mosaicplot(total, ylab = "Reef Area", xlab = "Color Morph", main = "")
@@ -103,21 +75,11 @@ results=glm(Color~Depth..m., family = "binomial", data = Symcap)
 anova(results, test = "Chisq")
 summary(results)
 plot(results)
-
 plot(Symcap$Dom~Symcap$Depth..m.)
- 
-exp(coef(results)) #how odds change
-exp(confint.default(results)) #95% confidence interval
-pi.hat=predict.glm(results, data.frame(Depth..m.=5.1), type = "response", se.fit = TRUE) #predict probability
-pi.hat$fit
-I.hat=predict.glm(results, data.frame(Depth..m.=5.1), se.fit = TRUE) #95% confidence interval for estimate
-ci=c(I.hat$se.fit-1.96*I.hat$se.fit, I.hat$fit+1.96*I.hat$se.fit)
-exp(ci)/(1+exp(ci)) #transform results to probabilities 
 
 #Plot Color Morph and Depth
 Symcap$Color <- ifelse(Symcap$Color.Morph=="Orange", 1, 0)
 plot(Symcap$Color~Symcap$Depth..m., xlab="Depth (m)", ylab = "Proportion of Color Morph")
-Symcap$Dom <- as.factor(Symcap$Dom)
 results=glm(Color~Depth..m., family = "binomial", data = Symcap)
 anova(results, test = "Chisq")
 summary(results)
@@ -125,9 +87,8 @@ fitted <- predict(results, newdata = list(Depth..m.=seq(0,8,0.1)), type = "respo
 lines(fitted ~ seq(0,8,0.1))
 
 #Plot Dominant Symbiont and Depth
-Symcap$Dominant <- ifelse(Symcap$Dom=="C", 1, 0)
+Symcap$Dominant <- ifelse(Symcap$Dom=="C", 0, 1)
 plot(Symcap$Dominant~Symcap$Depth..m., xlab="Depth (m)", ylab = "Proportion of Dominant Symbiont")
-Symcap$Dom <- as.factor(Symcap$Dom)
 results=glm(Dominant~Depth..m., family = "binomial", data = Symcap)
 anova(results, test = "Chisq")
 summary(results)
@@ -135,17 +96,27 @@ fitted <- predict(results, newdata = list(Depth..m.=seq(0,8,0.1)), type = "respo
 lines(fitted ~ seq(0,8,0.1))
 
 #Logistic Regression/Histogram Plot
-logi.hist.plot(Symcap$Depth..m., Symcap$Color, boxp = FALSE, type = "hist", col="gray", xlabel = "Depth (m)", ylabel = "", ylabel2 = "")
+logi.hist.plot(independ = Symcap$Depth..m., depend = Symcap$Color, type = "hist", boxp = FALSE, ylabel = "", col="gray", ylabel2 = "", xlabel = "Depth (m)")
 mtext(side = 4, text = "Frequency", line = 3, cex=1)
 mtext(side = 4, text = "Brown                                Orange", line = 2, cex = 0.75)
-mtext(side = 2, text = "Probability of Brown Color Morph", line = 3, cex = 1)
+mtext(side = 2, text = "Probability of Orange Color Morph", line = 3, cex = 1)
 
-
+Dom1 <- subset(Symcap, Dominant!="NA")
 logi.hist.plot(Dom1$Depth..m., Dom1$Dominant, boxp = FALSE, type = "hist", col="gray", xlabel = "Depth (m)", ylabel = "", ylabel2 = "")
 mtext(side = 4, text = "Frequency", line = 3, cex=1)
 mtext(side = 4, text = "C                                     D", line = 2, cex = 0.75)
 mtext(side = 2, text = "Probability of clade D Symbiont", line = 3, cex = 1)
 
+#Export Image
+pdf(file="Color~Depth.pdf", height = 4, width = 5)
+par(mar=c(4, 4, 4, 4))
+logi.hist.plot(Symcap$Depth..m., Symcap$Color, boxp = FALSE, type = "hist", col="gray", xlabel = "Depth (m)", ylabel = "", ylabel2 = "")
+mtext(side = 4, text = "Frequency", line = 3, cex=1)
+mtext(side = 4, text = "Brown                                   Orange", line = 2, cex = 0.75)
+mtext(side = 2, text = "Probability of Orange Color Morph", line = 3, cex = 1)
+dev.off()
+
 #RGoogleMaps
 KB <- c(21.46323, -157.81248)
-KBMap <- GetMap(center = KB, zoom = 10, maptype = "satellite", SCALE = 2)
+KBMap <- GetMap(center = KB, zoom = 13, maptype = "satellite", SCALE = 2)
+PlotOnStaticMap(KBMap, Symcap$Latitude, Symcap$Longitude, col="red")
